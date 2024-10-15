967aebfe3360cd887a431544afecb8ef8a94715e
ajafry <ajafry@gmail.com>
2024-10-14 22:18:34 -0700

    Updated code to use Semantic Kernel

--- src/ContosoSuitesWebAPI/Services/DatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/DatabaseService.cs
@@ -1,5 +1,7 @@
 using System.Runtime.CompilerServices;
 using Microsoft.Data.SqlClient;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
 using ContosoSuitesWebAPI.Entities;
 
 namespace ContosoSuitesWebAPI.Services;
@@ -11,6 +13,9 @@ public class DatabaseService : IDatabaseService
     {
         DbConnectionString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_ContosoSuites")!;
     }
+
+    [KernelFunction]
+    [Description("Get all hotels.")]
     public async Task<IEnumerable<Hotel>> GetHotels()
     {
         var sql = "SELECT HotelID, HotelName, City, Country FROM dbo.Hotel";
@@ -36,6 +41,8 @@ public class DatabaseService : IDatabaseService
         return hotels;
     }
 
+    [KernelFunction]
+    [Description("Get all bookings for a single hotel.")]
     public async Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId)
     {
         var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID";
@@ -64,6 +71,8 @@ public class DatabaseService : IDatabaseService
         return bookings;
     }
 
+    [KernelFunction]
+    [Description("Get all bookings for a hotel by a minimum date.")]
     public async Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt)
     {
         var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID AND StayBeginDate >= @StayBeginDate";
@@ -92,4 +101,94 @@ public class DatabaseService : IDatabaseService
 
         return bookings;
     }
+
+    [KernelFunction]
+    [Description("Get all bookings that do not have associated hotel rooms.")]
+    public async Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms()
+        {
+            var sql = """
+                SELECT
+                    b.BookingID,
+                    b.CustomerID,
+                    b.HotelID,
+                    b.StayBeginDate,
+                    b.StayEndDate,
+                    b.NumberOfGuests
+                FROM dbo.Booking b
+                WHERE NOT EXISTS
+                    (
+                        SELECT 1
+                        FROM dbo.BookingHotelRoom h
+                        WHERE
+                            b.BookingID = h.BookingID
+                    );
+                """;
+            using var conn = new SqlConnection(
+                connectionString: DbConnectionString
+            );
+            conn.Open();
+            using var cmd = new SqlCommand(sql, conn);
+            using var reader = await cmd.ExecuteReaderAsync();
+            var bookings = new List<Booking>();
+            while (await reader.ReadAsync())
+            {
+                bookings.Add(new Booking
+                {
+                    BookingID = reader.GetInt32(0),
+                    CustomerID = reader.GetInt32(1),
+                    HotelID = reader.GetInt32(2),
+                    StayBeginDate = reader.GetDateTime(3),
+                    StayEndDate = reader.GetDateTime(4),
+                    NumberOfGuests = reader.GetInt32(5)
+                });
+            }
+            conn.Close();
+
+            return bookings;
+        }
+
+    [KernelFunction]
+    [Description("Get all bookings with more than one hotel room.")]
+    public async Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms()
+        {
+            var sql = """
+                SELECT
+                    b.BookingID,
+                    b.CustomerID,
+                    b.HotelID,
+                    b.StayBeginDate,
+                    b.StayEndDate,
+                    b.NumberOfGuests
+                FROM dbo.Booking b
+                WHERE
+                    (
+                        SELECT COUNT(1)
+                        FROM dbo.BookingHotelRoom h
+                        WHERE
+                            b.BookingID = h.BookingID
+                    ) > 1;
+                """;
+            using var conn = new SqlConnection(
+                connectionString: DbConnectionString
+            );
+            conn.Open();
+            using var cmd = new SqlCommand(sql, conn);
+            using var reader = await cmd.ExecuteReaderAsync();
+            var bookings = new List<Booking>();
+            while (await reader.ReadAsync())
+            {
+                bookings.Add(new Booking
+                {
+                    BookingID = reader.GetInt32(0),
+                    CustomerID = reader.GetInt32(1),
+                    HotelID = reader.GetInt32(2),
+                    StayBeginDate = reader.GetDateTime(3),
+                    StayEndDate = reader.GetDateTime(4),
+                    NumberOfGuests = reader.GetInt32(5)
+                });
+            }
+            conn.Close();
+
+            return bookings;
+        }    
 }
